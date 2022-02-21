43148edcce3dc7d92b5bc2283e1fb56188630b17
Gilles Coissac <gilles@ideographe.fr>
2022-02-21 13:01:51 +0100

    Extend parsing mechanism in python-symbols plugin and make some clean.

--- src/python-symbols/sources_inspect.py
+++ src/python-symbols/sources_inspect.py
@@ -20,12 +20,15 @@
 #
 """sources_inspect.py
 
-Analyze python sources file and return a hierarchical
-representation of the code with its functions, class
-and class methods definitions. The result is a json
-formatted string printed on stdout.
+Loads a python sources file, parse it as an ast.Module
+and write back pickled bytes of this ast tree to a file.
+The name of this file is printed to stdout.
+The call process should open as bytes the file path
+read on stdout, and upickled the data read to
+reconstruct the ast tree. It's the responsability
+of the call process to remove te generated file on disk.
 In case of errors, stdout will be empty, errors will
-be printed on stderr, and the script will exit with
+be logged on stderr, and the script will exit with
 a status code of 1.
 Warning: It is possible to crash the Python
 interpreter with a sufficiently large/complex string
@@ -37,101 +40,47 @@ import ast
 import logging
 import pickle
 import sys
-from collections import OrderedDict
-from pathlib import Path
-from typing import OrderedDict as OrderedDict_T
 import tempfile
+from pathlib import Path
 
 logger = logging.getLogger("sources_inspect.py")
 logger.setLevel(level=logging.INFO)
-handler = logging.FileHandler("/home/gilles/PYTHON.log")
-formatter = logging.Formatter("%(asctime)s - %(levelname)-8s: %(name)-12s -> %(message)s")
+handler = logging.StreamHandler(stream=sys.stderr)
+formatter = logging.Formatter(
+    "%(asctime)s - %(levelname)-8s: %(name)-12s -> %(message)s"
+)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-logger.info(f"module {__file__} imported...")
 
 def import_source(path: Path) -> ast.AST:
     """Return the python file located at 'path' as
     an ast.AST tree root node.
     """
     with open(path, encoding='utf-8', mode='r') as file:
-        # major = 3
-        # minor = 9
         source = file.read()
-        # tree = ast.parse(
-        #     source,
-        #     filename="<string>",
-        #     mode="eval",
-        #     feature_version=(major, minor),
-        #     type_comments=False
-        # )
     return ast.parse(source)
 
 
-def parse(ast_tree: ast.AST) -> OrderedDict_T[str, OrderedDict_T]:
-    """Visit the root ast tree recursivly and return
-    a tree as an OrderedDict of pertinent code object
-    elements.
-    """
-    root = OrderedDict()
-    ast.fix_missing_locations(ast_tree)
-    for node in ast.iter_child_nodes(ast_tree):
-        visit_node(node, root)
-    return root
-
-
-def visit_node(node: ast.AST, parent: OrderedDict_T) -> None:
-    """Visit the ast 'node' and fill the 'parent' OrderedDict
-    with the node name as key if the type of node is
-    of interest(function, class, etc...). Call recursivly
-    on node child until reaching end of tree.
-    """
-    childs = None
-    lineno = node.lineno if hasattr(node, "lineno") else "__"
-    col_offset = node.col_offset if hasattr(node, "col_offset") else "__"
-    logger.info(f"{lineno}:{col_offset} {ast.dump(node)}\n")
-    if isinstance(node, ast.FunctionDef):
-        parent[node.name] = {"type": "FunctionDef", "childs": {}}
-        childs = parent[node.name]["childs"]
-    elif isinstance(node, ast.ClassDef):
-        parent[node.name] = {"type": "ClassDef", "childs": {}}
-        childs = parent[node.name]["childs"]
-    if childs is not None:
-        for node in ast.iter_child_nodes(node):
-            visit_node(node, childs)
-
-
 def pickle_ast(node: ast.AST) -> bytes:
+    """Pickle dump as bytes node instance."""
     return pickle.dumps(node, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 def main():  # noqa
     try:
         path = Path(sys.argv[1])
-        logger.info(f"running {__file__}...{sys.argv[1]}")
         if path.is_file():
             ast_tree = import_source(path)
-            # result = parse(ast_tree)
-            logger.debug(ast.dump(ast_tree))
             result = pickle_ast(ast_tree)
         else:
-            logger.info(
-                f"Not a valide filename {path}.",
-                file=sys.stderr
-            )
+            logger.info(f"Not a valide filename {path}.")
             sys.exit(1)
     except IndexError:
-        logger.info(
-            "Need a filename as positional argument.",
-            file=sys.stderr
-        )
+        logger.info("Need a filename as positional argument.")
         sys.exit(1)
     except (ValueError, IOError) as err:
-        logger.info(
-            f"Can't read file {sys.argv[1]} as source code:\n{err}",
-            file=sys.stderr
-        )
+        logger.info(f"Can't read file {sys.argv[1]} as source code:\n{err}")
         sys.exit(1)
     except pickle.PickleError as err:
         logger.info(f"PICKLE ERROR: {err}")
@@ -140,21 +89,17 @@ def main():  # noqa
         logger.info(f"UNDEFINED ERROR: {err}")
         sys.exit(1)
     else:
-        # stdout = json.dumps(result)
-        # print(stdout, file=sys.stdout)
         try:
-            tmpdir = tempfile.gettempdir()
-            tmpdir = "/home/gilles/.tmp"
-            file_path = Path(tmpdir) / f"_PY_{id(result)}.pickle"
-            logger.info(f"write python object to: {file_path}")
+            tmpdir = Path(tempfile.gettempdir())
+            _dir = tmpdir / "_pickled"
+            _dir.mkdir(parents=True, exist_ok=True)
+            file_path = _dir / f"_PY_{id(result)}.pickle"
             with open(file_path, mode='wb') as file:
                 file.write(result)
             sys.stdout.write(str(file_path))
-            # sys.stdout.buffer.write(result)
         except OSError as err:
             logger.info(f"OUTPUT ERROR: {err}")
             sys.exit(1)
-        logger.info("sources_inspect exit with success")
         sys.exit(0)
 
 
